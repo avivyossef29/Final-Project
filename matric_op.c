f590849ea5ce099e5ba2897f5e6aa0a6c7b18e12
Aviv Yossef <avivyossef@nova.cs.tau.ac.il>
2023-08-29 12:00:09 +0300

    add print matrix func

--- matric_op.c
+++ matric_op.c
@@ -1,13 +1,25 @@
+#include <stdio.h>
+
 double** create_matrix(int,int);
-double** mal(**double,**double,int,int,int);
+void print_matrix(double**,int,int);
+double** mal(double**,double**,int,int,int);
 
 
 double** create_matrix(int n , int m){
-    temp = (double*)malloc(n*m*sizeof(double));
-    mat = (double**)malloc(n*sizeof(double*));
+    double * temp = (double*)malloc(n*m*sizeof(double));
+    double** mat = (double**)malloc(n*sizeof(double*));
     if((mat == NULL) || (temp == NULL)) return;
     for(int i = 0 ; i < n ; i++){
         mat[i] = temp + m*i;
     }
     return mat;
+}
+
+void print_matrix(double** mat , int n , int m){
+    for(int i = 0 ; i< n ; i++){
+        for(int j = 0 ; j < m ; j++ ){
+            printf("%d ",mat[i][j]);
+        }
+        printf("\n");
+    }
 }
