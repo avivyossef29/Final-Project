a69e16b8624b7ebfd58e8878b91ae4606dd8addb
Aviv Yossef <avivyossef@nova.cs.tau.ac.il>
2023-08-29 13:41:23 +0300

    add mul func and norm_matrix func

--- matric_op.c
+++ matric_op.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
 
 double** create_matrix(int,int);
 void print_matrix(double**,int,int);
-double** mal(double**,double**,int,int,int);
+void delete_matrix(double**,int,int);
+double** mul(double**,double**,int,int,int);
+double norm_matrix(double** , int ,int);
 
 
 double** create_matrix(int n , int m){
     double * temp = (double*)malloc(n*m*sizeof(double));
     double** mat = (double**)malloc(n*sizeof(double*));
-    if((mat == NULL) || (temp == NULL)) return;
+    if((mat == NULL) || (temp == NULL)) return NULL;
     for(int i = 0 ; i < n ; i++){
         mat[i] = temp + m*i;
     }
@@ -18,8 +23,39 @@ double** create_matrix(int n , int m){
 void print_matrix(double** mat , int n , int m){
     for(int i = 0 ; i< n ; i++){
         for(int j = 0 ; j < m ; j++ ){
-            printf("%d ",mat[i][j]);
+            printf("%f ",mat[i][j]);
         }
         printf("\n");
     }
+}
+
+void delete_matrix(double** mat,int n , int m ){
+    free(mat[0]);
+    free(mat);
+}
+
+// A is n*k and B is k*m 
+double** mul(double** A,double** B , int n , int k , int m){
+    double temp;
+    double** C = create_matrix(n,m);
+    for(int i = 0 ; i < n ; i++){
+        for(int j = 0 ; j < m ; j++){
+            temp = 0.0;
+            for(int cnt = 0; cnt < k ; cnt++ ){
+                temp += (A[i][cnt] * B[cnt][j]);
+            }
+            C[i][j] = temp;
+        }
+    }
+    return C;
+}
+
+double norm_matrix(double** mat , int n ,int m){
+    double result = 0;
+    for(int i = 0 ; i < n ; i++ ){
+        for(int j = 0 ; j < m ; j++){
+            result += pow(mat[i][j] ,2);
+        }
+    }
+    return sqrt(result);
 }
---
+++ test_matric_op.c
@@ -0,0 +1,18 @@
+
+#include "matric_op.c"
+
+
+void main(int argc , char* argv){
+    double** A = create_matrix(3,3);
+    for(int i = 0 ; i < 3 ; i++){
+        for(int j = 0; j<3; j++){
+            A[i][j] = 3*i+j;
+        }
+    }
+    print_matrix(A,3,3);
+    double** A_2 = mul(A,A,3,3,3);
+    print_matrix(A_2,3,3);
+    printf("%f\n" , norm_matrix(A,3,3));
+    delete_matrix(A,3,3);
+    delete_matrix(A_2,3,3);
+}
