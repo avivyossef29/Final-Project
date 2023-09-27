3d66de7c0e6bbbc81ed35e1807b64c6d28059d54
Eitamar Ben Shushan <eitamar.benshushan@gmail.com>
2023-09-28 00:13:00 +0300

    added necessary files

--- init_H.py
+++ init_H.py
@@ -1,9 +1,12 @@
 import math
 import random
+import sys
+import numpy as np
 
+np.random.seed(0)
 
 def rand_float(lower_bound, upper_bound):
-    return random.uniform(lower_bound, upper_bound)
+    return np.random.uniform(lower_bound, upper_bound)
 
 
 def entries_avg(W):
@@ -20,3 +23,16 @@ def init_H(W, n, k):
     upper_bound = 2 * math.sqrt(entries_avg(W) / k)
     H = [[rand_float(0, upper_bound) for _ in range(k)] for _ in range(n)]
     return H
+
+def print_matrix(mat):
+    for row in mat:
+        print(row)
+
+def main():
+    matrix = [[1, 2, 3],
+              [4, 5, 6],
+              [7, 8, 9]]
+    h = init_H(matrix, len(matrix), 3)
+    print_matrix(h)
+
+
---
+++ symnmf.py
@@ -0,0 +1,45 @@
+import sys
+import numpy as np
+import init_H
+#import symnmfmodule
+
+# Read N vectors from a file
+def process_txt(file_name):
+    matrix = []
+    with open(file_name, 'r') as file:
+        for line in file:
+            # Split each line into a list of values using ',' as the separator
+            values = line.strip().split(',')
+            # Convert the values from strings to floats
+            row = [float(value) for value in values]
+            matrix.append(row)
+    return matrix
+
+def print_matrix(matrix):
+    for row in matrix:
+        formatted_row = [f"{num:.4f}" for num in row]
+        formatted_row_str = ",".join(formatted_row)
+        print(formatted_row_str)
+
+def main():
+    assert(len(sys.argv) == 4)
+    K = sys.argv[1] # K is the number of clusters
+    goal = sys.argv[2]
+    file_path = sys.argv[3]
+    matrix = process_txt(file_path)
+    print_matrix(matrix)
+    if goal == "symnmf":
+        #this method should receive W and initial H as arguments and return the final H
+        symnmf() #this method should be defined in a C file
+    elif goal == "sym":
+        # this method should receive the data points matrix and return the similarity matrix
+        sym() #this method should be defined in a C file
+    elif goal == "ddg":
+        #this method should receive the data points matrix and return the diagonal matrix D
+        ddg() #this method should be defined in a C file
+    elif goal == "norm":
+        # this method should receive the data points matrix and return the normalized similarity matrix
+        norm() #this method should be defined in a C file
+
+main()
+
