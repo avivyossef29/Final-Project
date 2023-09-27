b179d211ca8c0a2cb31fe9ba3ab428e22d7cbe61
Eitamar Ben Shushan <eitamar.benshushan@gmail.com>
2023-09-28 00:26:28 +0300

    refactoring

--- python_files/symnmf.py
+++ python_files/symnmf.py
@@ -1,7 +1,10 @@
 import sys
-import numpy as np
+
 import init_H
-#import symnmfmodule
+import numpy as np
+
+
+# import symnmfmodule
 
 # Read N vectors from a file
 def process_txt(file_name):
@@ -15,31 +18,33 @@ def process_txt(file_name):
             matrix.append(row)
     return matrix
 
+
 def print_matrix(matrix):
     for row in matrix:
         formatted_row = [f"{num:.4f}" for num in row]
         formatted_row_str = ",".join(formatted_row)
         print(formatted_row_str)
 
+
 def main():
-    assert(len(sys.argv) == 4)
-    K = sys.argv[1] # K is the number of clusters
+    assert (len(sys.argv) == 4)
+    K = sys.argv[1]  # K is the number of clusters
     goal = sys.argv[2]
     file_path = sys.argv[3]
     matrix = process_txt(file_path)
     print_matrix(matrix)
     if goal == "symnmf":
-        #this method should receive W and initial H as arguments and return the final H
-        symnmf() #this method should be defined in a C file
+        # this method should receive W and initial H as arguments and return the final H
+        symnmf()  # this method should be defined in a C file
     elif goal == "sym":
         # this method should receive the data points matrix and return the similarity matrix
-        sym() #this method should be defined in a C file
+        sym()  # this method should be defined in a C file
     elif goal == "ddg":
-        #this method should receive the data points matrix and return the diagonal matrix D
-        ddg() #this method should be defined in a C file
+        # this method should receive the data points matrix and return the diagonal matrix D
+        ddg()  # this method should be defined in a C file
     elif goal == "norm":
         # this method should receive the data points matrix and return the normalized similarity matrix
-        norm() #this method should be defined in a C file
+        norm()  # this method should be defined in a C file
+
 
 main()
-
